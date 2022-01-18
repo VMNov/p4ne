61f049c48c5be626370650b6556c478d74b2ea26
VMNov <novikov_v_m@mail.ru>
2022-01-18 18:59:48 +0300

    Lab1.2 add comments

--- Lab1.2/lab1.2.py
+++ Lab1.2/lab1.2.py
@@ -1,12 +1,14 @@
-from matplotlib import pyplot
-from openpyxl import load_workbook
-wb = load_workbook('data_analysis_lab.xlsx')
-sheet = wb['Data']
+from matplotlib import pyplot # Импортировать модуль pyplot из библиотеки matplotlib
+from openpyxl import load_workbook # Импортировать модуль load_workbook из библиотеки openpyxl
+wb = load_workbook('data_analysis_lab.xlsx') # Загрузить таблицу Excel из файла "data_analysis_lab.xlsx" в переменную wb
+sheet = wb['Data'] # Загрузить лист с именем "Data" в переменную sheet
+
 def getvalue(x):
     return x.value
-years = list(map(getvalue, sheet['A'][1:]))
-temp = list(map(getvalue, sheet['C'][1:]))
-sun = list(map(getvalue, sheet['D'][1:]))
-pyplot.plot(years, temp, label="Температура")
-pyplot.plot(years, sun, label="Активность солнца")
-pyplot.show()
+years = list(map(getvalue, sheet['A'][1:])) # Преобразовать содержимое колонки A в список в переменной years, с помощью list и map
+temp = list(map(getvalue, sheet['C'][1:])) # Преобразовать содержимое колонки С в список в переменной temp,с помощью list и map
+sun = list(map(getvalue, sheet['D'][1:])) # Преобразовать содержимое колонки D в список в переменной sun, с помощью list и map
+pyplot.plot(years, temp, label="Температура") # Построить график по точкам X=years, Y=temp
+pyplot.plot(years, sun, label="Активность солнца") # Построить график по точкам X=years, Y=sun
+pyplot.show() # Показать график
+
